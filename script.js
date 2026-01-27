0852e4a42d92159013b03ff8b9ce04f97a250908
Marcelo Carneiro <marcelo_tech100@hotmail.com>
2026-01-27 20:58:49 -0300

    Add script to get entered value from amount input

---
+++ script.js
@@ -0,0 +1,9 @@
+const amount = document.getElementById("amount")
+
+
+// TODO - Manipulando o input amount para receber somente números
+amount.addEventListener("input", () => {
+
+    // Obtendo o valor digitado no input amount
+    console.log(amount.value)
+})
