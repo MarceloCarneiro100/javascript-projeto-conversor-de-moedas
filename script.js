d236bd006a9e46e70a6a8a026feaee308920a3e5
Marcelo Carneiro <marcelo_tech100@hotmail.com>
2026-01-27 21:18:34 -0300

    Get selected currency

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
+// Obtendo os elementos do formulário
+const form = document.querySelector("form")
 const amount = document.getElementById("amount")
+const currency = document.getElementById("currency")
 
 
 // Manipulando o input amount para receber somente números
 amount.addEventListener("input", () => {
-
     const hasCharactersRegex = /\D+/g
     amount.value = amount.value.replace(hasCharactersRegex, "")
 })
+
+// Capturando o evento de submit do formulário
+form.onsubmit = (event) => {
+   // Desativa o comportamento padrão de recarregamento da página
+   event.preventDefault()
+
+   // Obtém a moeda selecionada
+   console.log(currency.value)
+}
